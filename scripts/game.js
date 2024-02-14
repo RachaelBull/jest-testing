fe6c64a569f56e9bf73398f4359e07cbf1cc89e2
Rachael Bull <rachaelbull2021@gmail.com>
2024-02-14 16:05:41 +0000

    Finish todays videos

--- scripts/game.js
+++ scripts/game.js
@@ -1,7 +1,9 @@
 let game = {
-    score: 0,
     currentGame: [],
-}
+    playerMoves: [],
+    score: 0,
+    choices: ["button1", "button2", "button3", "button4"]
+};
 
 
 module.exports = { game };
